71d256f22814132028837bf670a14fd965ce4f0f
Justus Adam <justbldr@amazon.com>
2023-06-22 13:20:48 -0700

    Documentation

--- library/kani/src/lib.rs
+++ library/kani/src/lib.rs
@@ -66,6 +66,7 @@ pub fn assume(cond: bool) {
     assert!(cond, "`kani::assume` should always hold");
 }
 
+/// If the `premise` is true, so must be the `conclusion`
 pub fn implies(premise: bool, conclusion: bool) -> bool {
     !premise || conclusion
 }
