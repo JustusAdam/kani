b865265ea7586aab09eff0abeeeba443955d36cb
Justus Adam <justbldr@amazon.com>
2023-07-07 11:45:50 -0700

    Fixing serialization for multi-contracts

--- cprover_bindings/src/irep/to_irep.rs
+++ cprover_bindings/src/irep/to_irep.rs
@@ -584,11 +584,11 @@ impl ToIrep for Lambda {
             sub: vec![Irep::just_sub(types), self.body.typ().to_irep(mm)],
             named_sub: Default::default(),
         };
-        Irep::just_sub(vec![Irep {
+        Irep {
             id: IrepId::Lambda,
             sub: vec![Irep::tuple(ops_ireps), self.body.to_irep(mm)],
             named_sub: linear_map!((IrepId::Type, typ)),
-        }])
+        }
     }
 }
 
@@ -596,15 +596,18 @@ impl goto_program::Symbol {
     pub fn to_irep(&self, mm: &MachineModel) -> super::Symbol {
         let mut typ = self.typ.to_irep(mm);
         if let Some(contract) = &self.contract {
-            for requires in &contract.requires {
-                typ = typ.with_named_sub(IrepId::CSpecRequires, requires.to_irep(mm));
-            }
-            for ensures in &contract.ensures {
-                typ = typ.with_named_sub(IrepId::CSpecEnsures, ensures.to_irep(mm));
-            }
-            for assigns in &contract.assigns {
-                typ = typ.with_named_sub(IrepId::CSpecAssigns, assigns.to_irep(mm));
-            }
+            typ = typ.with_named_sub(
+                IrepId::CSpecRequires,
+                Irep::just_sub(contract.requires.iter().map(|req| req.to_irep(mm)).collect()),
+            );
+            typ = typ.with_named_sub(
+                IrepId::CSpecEnsures,
+                Irep::just_sub(contract.ensures.iter().map(|req| req.to_irep(mm)).collect()),
+            );
+            typ = typ.with_named_sub(
+                IrepId::CSpecAssigns,
+                Irep::just_sub(contract.assigns.iter().map(|req| req.to_irep(mm)).collect()),
+            );
         }
         super::Symbol {
             typ,
