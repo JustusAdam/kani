0aa8b26eb45bd110eac55c2df3879408d810f256
Justus Adam <justbldr@amazon.com>
2023-08-21 14:21:43 -0700

    Tried reframing this example, still doesn't work

--- tests/expected/function-contract/fixme_contracts_crate_example.rs
+++ tests/expected/function-contract/fixme_contracts_crate_example.rs
@@ -4,19 +4,27 @@
 // kani-flags: -Zfunction-contracts
 extern crate kani;
 use kani::*;
-use std::collections::HashSet;
+use std::collections::BTreeSet;
+
+#[derive(Clone, Copy, Hash, Eq, PartialEq, PartialOrd, Ord)]
+pub enum Title {
+    DasKapital,
+    TheDiaryOfAnneFrank,
+    HiddenFigures,
+    WhenHeavenAndEarthChangedPlaces,
+}
 
 pub fn implies(premise: bool, conclusion: bool) -> bool {
     !premise || conclusion
 }
 
 pub struct Library {
-    available: HashSet<String>,
-    lent: HashSet<String>,
+    available: BTreeSet<Title>,
+    lent: BTreeSet<Title>,
 }
 
 impl Library {
-    fn book_exists(&self, book_id: &str) -> bool {
+    fn book_exists(&self, book_id: &Title) -> bool {
         self.available.contains(book_id) || self.lent.contains(book_id)
     }
 
@@ -24,8 +32,8 @@ impl Library {
     #[ensures(self.available.contains(book_id))]
     //#[ensures(self.available.len() == old(self.available.len()) + 1)]
     //#[ensures(self.lent.len() == old(self.lent.len()))]
-    pub fn add_book(&mut self, book_id: &str) {
-        self.available.insert(book_id.to_string());
+    pub fn add_book(&mut self, book_id: &Title) {
+        self.available.insert(*book_id);
     }
 
     #[requires(self.book_exists(book_id))]
@@ -33,10 +41,10 @@ impl Library {
     //#[ensures(implies(result, self.lent.len() == old(self.lent.len()) + 1))]
     #[ensures(implies(result, self.lent.contains(book_id)))]
     #[ensures(implies(!result, self.lent.contains(book_id)))]
-    pub fn lend(&mut self, book_id: &str) -> bool {
+    pub fn lend(&mut self, book_id: &Title) -> bool {
         if self.available.contains(book_id) {
             self.available.remove(book_id);
-            self.lent.insert(book_id.to_string());
+            self.lent.insert(*book_id);
             true
         } else {
             false
@@ -48,28 +56,35 @@ impl Library {
     //#[ensures(self.available.len() == old(self.available.len()) + 1)]
     #[ensures(!self.lent.contains(book_id))]
     #[ensures(self.available.contains(book_id))]
-    pub fn return_book(&mut self, book_id: &str) {
+    pub fn return_book(&mut self, book_id: &Title) {
         self.lent.remove(book_id);
-        self.available.insert(book_id.to_string());
+        self.available.insert(*book_id);
     }
 }
 
-#[kani::proof_for_contract(lend)]
 fn main() {
-    let mut lib = Library { available: HashSet::new(), lent: HashSet::new() };
+    let mut lib = Library { available: Default::default(), lent: Default::default() };
 
-    lib.add_book("Das Kapital");
-    println!("Adding a book.");
+    lib.add_book(&Title::DasKapital);
 
-    let lent_successful = lib.lend("Das Kapital");
+    let lent_successful = lib.lend(&Title::DasKapital);
     assert!(lent_successful);
 
     if lent_successful {
-        println!("Lent out the book.");
-        println!("Reading for a bit...");
 
-        println!("Giving back the book.");
-
-        lib.return_book("Das Kapital");
+        lib.return_book(&Title::DasKapital);
     }
+}
+
+
+#[kani::proof_for_contract(Library::lend)]
+#[kani::unwind(10)]
+fn lend_harness() {
+    main()
+}
+
+// #[kani::proof]
+// #[kani::stub_verified(Library::lend)]
+fn lend_replace() {
+    main()
 }
